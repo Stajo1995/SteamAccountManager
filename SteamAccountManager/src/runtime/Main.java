cdc099825216d2aaf720dd81665dc31ff6fb5d73
DEVSEC-SH_BREDA\Stilborgh <stevanti@hotmail.com>
2017-02-02 23:11:32 +0100

    Fixed a wrong button being used in the menu view.

--- SteamAccountManager/src/runtime/Main.java
+++ SteamAccountManager/src/runtime/Main.java
@@ -8,11 +8,10 @@ public class Main {
 	 * @author Stefan van T.
 	 * @reddit /u/Stevanti
 	 * @steam http://steamcommunity.com/id/Stajo/
-	 * @version 0.0.3
+	 * @version 0.0.4
 	 * @license MIT
 	 **/
 
-	// TODO: All exception catches should be properly handled with popups for the user.
 	public static void main(String[] args) {
 		new Engine();
 	}
